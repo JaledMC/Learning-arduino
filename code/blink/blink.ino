eb0e19b0f788b064529046ecea51f186e42ec306
JaledMC <j.moustafacalvo@gmail.com>
2018-12-08 13:05:04 +0100

    Update blink.ino

--- code/blink/blink.ino
+++ code/blink/blink.ino
@@ -1,12 +1,17 @@
-//el setup funciona una única vez al principio del programa o reiniciarse el arduino. Sirve //para configurar la funcion de los pines generalmente. 
+const led = 13 // we can assign a name to the pin. 
+//the setup works only one time at the beginning of the program, at startup, usually for pin configurations.
 void setup() {                
-  // configuramos el pin 13 como salida (OUTPUT), con la 
-  pinMode(13, OUTPUT);     
+  // configure output 13 like output 
+  # pinMode(13, OUTPUT);
+  pinMode(led, OUTPUT);
 }
-//la funcion loop se ejecuta una y otra vez mientras este encendido el arduino
+//
+//loop function repeats forever while arduino is on. When last sentence ends, first line start again.
 void loop() {
-  digitalWrite(13, HIGH);   // digitalWrite con HIGH activa el pin 2, encendiendo el led.
+  # digitalWrite(13, HIGH);   // digitalWrite con HIGH activa el pin 2, encendiendo el led.
+  digitalWrite(led, HIGH);   // digitalWrite con HIGH activa el pin 2, encendiendo el led.
   delay(1000);               // esperamos 1 segundo con el led encendido. El arduino se bloquea
-  digitalWrite(13, LOW);    // digitalWrite con LOW desactiva el pin 2, apagando el led.
+  # digitalWrite(13, LOW);    // digitalWrite con LOW desactiva el pin 2, apagando el led.
+  digitalWrite(led, HIGH);   // digitalWrite con HIGH activa el pin 2, encendiendo el led.
   delay(1000);               // esperamos 1 segundo con el led encendido. El arduino se bloquea
 }
