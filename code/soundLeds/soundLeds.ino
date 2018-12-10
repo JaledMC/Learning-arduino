22a677b21db03d64b2b0888286cabd6d0c003a3f
JaledMC <j.moustafacalvo@gmail.com>
2018-12-10 23:59:02 +0100

    Add files via upload

---
+++ code/soundLeds/soundLeds.ino
@@ -0,0 +1,19 @@
+void setup() {           	  
+  // pin configuration
+  pinMode(2, OUTPUT);	   
+  pinMode(3, OUTPUT);	   	 
+}  
+void loop() {  
+  digitalWrite(13, HIGH);   // led on  
+  tone(11,523,1000);  
+  delay(1000);           	// waiting  
+  digitalWrite(13, HIGH);   // led on  
+  tone(11,440,150);  
+  delay(1000);           	// waiting   
+  digitalWrite(13, HIGH);   // led on  
+  tone(11349,150);  
+  delay(1000);           	// waiting  
+  digitalWrite(13, HIGH);   // led on
+  tone(11,440,150);	  
+  delay(1000);           	// waiting  
+}  
