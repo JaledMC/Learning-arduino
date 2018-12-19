e9a089f60f8db47050790286742874727363a3a7
JaledMC <j.moustafacalvo@gmail.com>
2018-12-19 13:46:44 +0100

    Update analog_read.ino

--- code/analog_read/analog_read.ino
+++ code/analog_read/analog_read.ino
@@ -1,18 +1,15 @@
 void setup() {
-  //fijamos la comunicación en 9600 bits por segundo. Se pueden poner otras velocidades:
-  //300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, o 115200, pero 
-  //es importante que el ordenador y el arduino vayan siempre a la misma velocidad, o no se
-  //entenderan (fallo tipico con impresoras)
-
-
+  //Serial communication speed configured to 9600. Other values can be used: 
+  //300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, o 115200, but both devices (arduino and pc),
+  //should have the same speed.
   Serial.begin(9600);
 }
 
 
 void loop() {
-  // leemos el valor analogico en el pin A0, y la guardamos en una variable tipo int:
+  // Read analog value on pin A0, and save the value in an int variable
   int valorSensor = analogRead(A0);
-  // Vemos en pantalla el valor del sensor con la funcion serial.println
+  // Show the variable value on the screen with serial.println
   Serial.println(valorSensor );
-  delay(10);        // esperamos un tiempo hasta la siguiente lectura
+  delay(100);        // wait some time
 }
