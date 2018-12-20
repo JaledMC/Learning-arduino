f132387ab0eb1bd71d8ac28e57ee92527fa6e629
JaledMC <j.moustafacalvo@gmail.com>
2018-12-20 13:33:36 +0100

    Update position_detector.ino

--- code/position_detector/position_detector.ino
+++ code/position_detector/position_detector.ino
@@ -1,19 +1,21 @@
-//incluimos la libreria servo
+//import servo library
 #include <Servo.h>
 
 
-Servo myservo;  // creamos un objeto de tipo servo.
-int pos;    // variable que guarda el valor del sensor para ser volcado en el servo
+Servo myservo;  // object servo type instance. Read object oriented programming for more info
+int pos;    // this variable stores the potentiometer position
 
 
 void setup() {
-  myservo.attach(9);  //el servo”myservo” le conectamos al pin 9
+  myservo.attach(9);  //connect myservo to pin 9
 }
-
 
 void loop() {
-  int val = analogRead(0);            //leemos el valor del potenciometro, entre 0 y 1023
-  pos = map(val, 0, 1023, 0, 180);     // escalamos pos entre 0 y 180
-  myservo.write(pos);                  // damos la orden de movimiento al servo
-  delay(15);                           // esperamos a que llegue a la posicion ordenada
+  int val = analogRead(0);             // read potentiometer on pin 0. Value goes from 0 to 1023
+  pos = map(val, 0, 1023, 0, 120);     // Use map to scale the pot val from 0 to 120 (servo range)
+  myservo.write(pos);                  // move the servo to the desired position
+  delay(15);                           // give to the servo some time to move 
 }
+
+
+
